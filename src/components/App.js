e5ba4edf95263aefb8ec47bc91a857ef29468be3
Edward Suwirya <edward.suwirya@ffi.co.id>
2019-09-17 11:54:49 +0700

    Section 128 Udemy Modern React with Redux

--- src/components/App.js
+++ src/components/App.js
@@ -4,23 +4,45 @@ import youtube from '../api/youtube';
 import youtubeParams from '../api/youtubeParams';
 
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
-    state = { videos: [] };
+    state = { videos: [], selectedVideo: null };
     onTermSubmit = async (term) => {
         youtubeParams['q'] = term;
         const response = await youtube.get('/search', {
             params: youtubeParams
         })
-        this.setState({ videos: response.data.items })
+        this.setState({
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        })
     };
+
+    onVideoSelect = (video) => {
+        this.setState({ selectedVideo: video });
+    }
+
+    componentDidMount() {
+        this.onTermSubmit('youtube');
+    }
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoList videos={this.state.videos} />
-            </div>
+                <div className="ui grid">
+                    <div className="row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
 
+                        <div className="five wide column">
+                            <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         )
     };
 }
